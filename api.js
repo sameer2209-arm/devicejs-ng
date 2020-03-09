114c99fc4e81b54a3420e62c6f771c8c72fc0849
Sameer Soin <sameer.soin@arm.com>
2020-03-09 21:16:59 +0530

    Updates

---
+++ api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "DeviceJSPeer",
+        "ResourceSelection"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
